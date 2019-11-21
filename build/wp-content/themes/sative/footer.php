e9e7d6e3967db5626bb4a8ce860556b8a55514b4
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2019-11-21 11:00:25 +0100

    another

--- build/wp-content/themes/sative/footer.php
+++ build/wp-content/themes/sative/footer.php
@@ -12,54 +12,55 @@
 ?>
             <footer class="footer">
                 <div class="footer__top">
-
+                    <div class="row justify-content-center">
+                        <div class="col-lg-3">
+                            <a href="/" class="logo">
+                                <object type="image/svg+xml" data="<?= get_template_directory_uri(); ?>/assets/img/logo.svg">
+                                    <img src="<?= get_template_directory_uri(); ?>/assets/img/logo.png" alt="">
+                                </object>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                    <div class="col-lg-4">
+                            <h4>Kontakt</h4>
+                        </div>
+                        <div class="col-lg-4">
+                            <h4>Mapa strony</h4>
+                            <?php
+                                wp_nav_menu(array(
+                                    'theme_location'    => 'primary',
+                                    'container'       => '',
+                                    'container_id'    => '',
+                                    'container_class' => '',
+                                    'menu_id'         => false,
+                                    'menu_class'      => 'main-menu',
+                                    'depth'           => 2,
+                                    'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
+                                    'walker'          => new wp_bootstrap_navwalker()
+                                ));
+                            ?>
+                        </div>
+                        <div class="col-lg-4">
+                            <h4>Pomoc</h4>
+                            <?php
+                                wp_nav_menu(array(
+                                    'theme_location'    => 'help',
+                                    'container'       => '',
+                                    'container_id'    => '',
+                                    'container_class' => '',
+                                    'menu_id'         => false,
+                                    'menu_class'      => 'help-menu',
+                                    'depth'           => 2,
+                                    'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
+                                    'walker'          => new wp_bootstrap_navwalker()
+                                ));
+                            ?>
+                        </div>
+                    </div>
                 </div>
                 <div class="footer__bottom">
                     <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-3">
-                                <a href="/" class="logo">
-                                    <object type="image/svg+xml" data="<?= get_template_directory_uri(); ?>/assets/img/logo.svg">
-                                        <img src="<?= get_template_directory_uri(); ?>/assets/img/logo.png" alt="">
-                                    </object>
-                                </a>
-                            </div>
-                            <div class="col-lg-4">
-                                <h4>Kontakt</h4>
-                            </div>
-                            <div class="col-lg-4">
-                                <h4>Mapa strony</h4>
-                                <?php
-                                    wp_nav_menu(array(
-                                        'theme_location'    => 'primary',
-                                        'container'       => '',
-                                        'container_id'    => '',
-                                        'container_class' => '',
-                                        'menu_id'         => false,
-                                        'menu_class'      => 'main-menu',
-                                        'depth'           => 2,
-                                        'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
-                                        'walker'          => new wp_bootstrap_navwalker()
-                                    ));
-                                ?>
-                            </div>
-                            <div class="col-lg-4">
-                                <h4>Pomoc</h4>
-                                <?php
-                                    wp_nav_menu(array(
-                                        'theme_location'    => 'help',
-                                        'container'       => '',
-                                        'container_id'    => '',
-                                        'container_class' => '',
-                                        'menu_id'         => false,
-                                        'menu_class'      => 'help-menu',
-                                        'depth'           => 2,
-                                        'fallback_cb'     => 'wp_bootstrap_navwalker::fallback',
-                                        'walker'          => new wp_bootstrap_navwalker()
-                                    ));
-                                ?>
-                            </div>
-                        </div>
                         <div class="row justify-content-between align-items-center">
                             <div class="col-auto">
                                 Copyright &copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
