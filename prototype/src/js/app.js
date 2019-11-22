8bead363568c13a456d0720f9fe8c909fab8393a
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2019-11-22 11:33:58 +0100

    asdfasdfasdfsadf

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -417,7 +417,7 @@ $(document).ready(function() {
 		}
 		var $dates = $orderDO['dates'].join(';');
 		var $meals = $orderDO['meals'].join(';');
-		var $finalURL = $base+$productID+'&quantity='+$quantity+'&variation_id='+$orderDO['variationID']+$variationsString+'&dates='+$dates+'&hour='+$orderDO['hour']+'&meals='+$meals;
+		var $finalURL = $base+$productID+'&quantity='+$quantity+'&variation_id='+$orderDO['variationID']+$variationsString+'&dates='+$dates;
 
 		console.log($orderDO);
 
