25e906032025f53e2430eabce182325e6dde5f53
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2019-11-23 00:01:56 +0100

    amsll

--- build/wp-content/themes/sative/tpl_about.php
+++ build/wp-content/themes/sative/tpl_about.php
@@ -6,6 +6,24 @@
 get_header();
 ?>
 
+<header class="header header__diets">
+    <picture class="bg-cover-abs">
+        <source class="lazyset bg-cover-abs" media="(min-width: 1600px)" data-srcset="<?= get_the_post_thumbnail_url(); ?>">
+        <source class="lazyset bg-cover-abs" media="(min-width: 1024px)" data-srcset="<?= get_the_post_thumbnail_url('', '1536x1536'); ?>">
+        <source class="lazyset bg-cover-abs" media="(min-width: 480px)" data-srcset="<?= get_the_post_thumbnail_url('', 'large'); ?>">
+        <source class="lazyset bg-cover-abs" media="(min-width: 1px)" data-srcset="<?= get_the_post_thumbnail_url('', 'medium_large'); ?>">
+        <img class="lazy bg-cover-abs" data-src="<?= get_the_post_thumbnail_url(); ?>" alt="">
+    </picture>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-8 col-12">
+                <h1>
+                    <?= get_the_title(); ?>
+                </h1>
+            </div>
+        </div>
+    </div>
+</header>
 
 <?php
 get_footer();
