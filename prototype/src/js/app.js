d52dea0a4863eca92ff461098651f60e9599e356
Jakub Pabis <jakubpabis@jakubs-macbook-pro.home>
2019-11-24 00:27:06 +0100

    sdfgsdfgh

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -445,7 +445,7 @@ $(document).ready(function() {
 	$(document).on('click', '.woocommerce-notices-wrapper', function(e) {
 		var $el = e.target;
 		console.log($el);
-		if($el.attr('class') == 'woocommerce-notices-wrapper'){
+		if($($el).hasClass('woocommerce-notices-wrapper')){
 			$(this).removeClass('vis');
 		}
 	});
