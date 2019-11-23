23d0e10b117450cd141196077d2358d8018a1aa9
Jakub Pabis <jakubpabis@jakubs-macbook-pro.home>
2019-11-23 22:06:04 +0100

    asdf

--- build/wp-content/themes/sative/tpl_contact.php
+++ build/wp-content/themes/sative/tpl_contact.php
@@ -10,5 +10,18 @@ get_header();
     <iframe src="<?= get_field('map') ?>" frameborder="0" style="border:0" allowfullscreen></iframe>
 </header>
 
+<section class="contact__section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <?= get_field('text'); ?>
+            </div>
+            <div class="col-lg-6">
+                <?= do_shortcode('[contact-form-7 id="6" title="Contact form 1"]'); ?>
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php
 get_footer();
