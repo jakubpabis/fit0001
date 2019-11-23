3f5954d822ef61cc803e152d87fafb46d8b67c61
Jakub Pabis <jakubpabis@jakubs-macbook-pro.home>
2019-11-23 21:36:23 +0100

    asdf

--- build/wp-content/themes/sative/tpl_contact.php
+++ build/wp-content/themes/sative/tpl_contact.php
@@ -6,6 +6,9 @@
 get_header();
 ?>
 
+<header class="header header__about">
+    <iframe src="<?= get_field('map') ?>" frameborder="0" style="border:0" allowfullscreen></iframe>
+</header>
 
 <?php
 get_footer();
