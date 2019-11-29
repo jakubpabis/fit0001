8ca46bf9e6a9b1f741e0457a05c1a9f29abf3120
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2019-11-29 10:05:52 +0100

    zamiany mapy na zdjecie

--- build/wp-content/themes/sative/tpl_about.php
+++ build/wp-content/themes/sative/tpl_about.php
@@ -8,17 +8,17 @@ get_header();
 
 <header class="header header__about">
     <picture class="bg-cover-abs">
-        <source class=" bg-cover-abs" media="(min-width: 1600px)" srcset="<?= get_the_post_thumbnail_url(); ?>">
-        <source class=" bg-cover-abs" media="(min-width: 1024px)" srcset="<?= get_the_post_thumbnail_url('', '1536x1536'); ?>">
-        <source class=" bg-cover-abs" media="(min-width: 480px)" srcset="<?= get_the_post_thumbnail_url('', 'large'); ?>">
-        <source class=" bg-cover-abs" media="(min-width: 1px)" srcset="<?= get_the_post_thumbnail_url('', 'medium_large'); ?>">
-        <img class=" bg-cover-abs" src="<?= get_the_post_thumbnail_url(); ?>" alt="">
+        <source class=" bg-cover-abs" media="(min-width: 1600px)" srcset="<?= get_field('header_image')['url']; ?>">
+        <source class=" bg-cover-abs" media="(min-width: 1024px)" srcset="<?= get_field('header_image')['sizes']['1536x1536']; ?>">
+        <source class=" bg-cover-abs" media="(min-width: 480px)" srcset="<?= get_field('header_image')['sizes']['large']; ?>">
+        <source class=" bg-cover-abs" media="(min-width: 1px)" srcset="<?= get_field('header_image')['sizes']['medium_large']; ?>">
+        <img class=" bg-cover-abs" src="<?= get_field('header_image')['url']; ?>" alt="">
     </picture>
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-8 col-12">
                 <h1>
-                    <?= get_the_title(); ?>
+                    <?= get_field('header_title'); ?>
                 </h1>
             </div>
         </div>
